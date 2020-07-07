3a50d6dc43cd17fd766b97da99e861c90f61e63f
Kim Lew <kimlew451@gmail.com>
2020-07-07 09:53:46 -0600

    Corrects condition & comments, cleans up file

--- rename_files.sh
+++ rename_files.sh
@@ -4,18 +4,23 @@
 #
 # Brief: Command-line Bash script to remove common text in multiple LibreOffice
 # .odt filenames in 1 directory - so filenames are shorter & easier-to-read.
+
 # Takes in 3 parameters at command line, e.g.,
 # Directory Path:       /Users/kimlew/Sites/bash_projects/test_rename_files
 # Old Text:    'Inkscape Essent Train-'
 # New Text:   nothing or ''
-#
+
+# OR gives user prompts if 0 arguments given.
+
 # Author: Kim Lew
 
 check_for_arguments() {
-  # Check if command-line arguments given. 
-  # If no  given, display user prompts.
-  # If has arguments, passes in the all args with $@.
-  
+  # Check if command-line arguments given. If user gives:
+  # 3+ arguments - see message with hint that this is wrong.
+  # 3 arguments - passes in the all args with $@.
+  # 1 or 2 arguments - see message with hint of what is wrong.s
+  # 0 arguments - display user prompts for each of 3 required inputs.
+
   if [ $# -gt 3 ]; then 
     # Case of 4 or more parameters given.
     echo "Give 3 command-line arguments. Or give 0 arguments & get prompts."
@@ -56,7 +61,6 @@ check_for_arguments() {
 }
 
 print_arguments() {
-  # Use the ${parameter%word} expansion to remove trailing \ from path.
   directory_path=${directory_path}
 
   echo "Checking:"
@@ -77,7 +81,7 @@ check_directory() {
 }
 
 need_single_quotes="If argument has SPACES, place argument within single quotes."
-check_for_arguments "$@" # For the actual arguments. # For number of args, use $#.
+check_for_arguments "$@"
 print_arguments
 
 echo "Checking files..."
@@ -88,15 +92,10 @@ while read -r a_file_name; do
   # Replace only 1st match: ${string/pattern/replacement}
   # NOTE: Tricky since pattern must be in the specific format above & CANNOT be 
   # a variable.
+  # -e - flag for indicating the pattern you want to match against.
 
-  # TODO: grep'/path/to/somewhere/' -e 'pattern'
-  # -e is the flag for indicating the pattern you want to match against
-  # B4: Checking if $old_text is in file path. grep looks INSIDE files.
-  # Now: B/c of |, matching stdin of grep stuff & outputting with echo command.
-  # if (grep "$a_file_name" -e "${old_text}"); then
-  if (find . -maxdepth 1 -type f -name '*"${old_text}"*.odt'); then
-    # mac$ echo "A Test-Ch 0b Test.odt" | grep -e 'A Test-'
-    echo "FOUND $old_text in $a_file_name"
+  if (echo "$a_file_name" | grep -q -e "$old_text"); then
+    echo "FOUND $old_text IN:" "$(basename "$a_file_name")"
     new_file_name="${a_file_name/$old_text/$new_text}"
     mv "$a_file_name" "$new_file_name"
     file_sort_counter="$((file_sort_counter+1))"
@@ -107,7 +106,7 @@ echo "DONE. Number of files renamed: " "${file_sort_counter}"
 
 end=$(date +%s)
 difference=$((end - start))
-echo "Renaming files took:" $((difference/60)) "min(s)" $((difference%60)) "sec(s)" 
+echo "Processing Time:" $((difference/60)) "min(s)" $((difference%60)) "sec(s)" 
 
 if [ "${file_sort_counter}" -eq 0 ]; then
   echo "$old_text was NOT found in filenames or there are NO .odt files at the top-level of the path."
